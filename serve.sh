b9147f36e74c6efbef7180b4d3b8deb0cc59faa2
psychowood <psychowood@gmail.com>
2016-08-09 18:04:48 +0200

    Added serve.sh script

---
+++ serve.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+tmpdir=`mktemp -d`
+
+echo ''
+echo 'Starting HENkaku server, CTRC+c to stop when you are done'
+echo ''
+javac android/app/src/main/java/com/psychowood/henkaku/HENprocess.java  android/app/src/main/java/com/psychowood/henkaku/HenkakuWebServer.java android/app/src/main/java/fi/iki/elonen/NanoHTTPD.java -d $tmpdir
+java -cp $tmpdir:android/app/src/main/resources com.psychowood.henkaku.HenkakuWebServer android/app/src/main/assets/ $1
+echo ''
+rm -R $tmpdir
+echo "done" >&2
