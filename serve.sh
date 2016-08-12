a55ff9100c797ce9959919f2d1ff8c7c93aa074c
psychowood <psychowood@gmail.com>
2016-08-12 19:03:57 +0200

    Moved henkaku files to assets/henkaku

--- serve.sh
+++ serve.sh
@@ -6,7 +6,7 @@ echo ''
 echo 'Starting HENkaku server, CTRC+c to stop when you are done'
 echo ''
 javac android/app/src/main/java/com/psychowood/henkaku/HENprocess.java  android/app/src/main/java/com/psychowood/henkaku/HenkakuWebServer.java android/app/src/main/java/fi/iki/elonen/NanoHTTPD.java -d $tmpdir
-java -cp $tmpdir:android/app/src/main/resources com.psychowood.henkaku.HenkakuWebServer android/app/src/main/assets/ $1
+java -cp $tmpdir:android/app/src/main/resources com.psychowood.henkaku.HenkakuWebServer android/app/src/main/assets/henkaku/ $1
 echo ''
 rm -R $tmpdir
 echo "done" >&2
