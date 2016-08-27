dab31805c2d08f60f7fc1247b98bf7a4a2df77be
psychowood <psychowood@gmail.com>
2016-08-28 01:43:29 +0200

    Added preferences constants

---
+++ android/app/src/main/java/com/psychowood/yahapp/App.java
@@ -0,0 +1,12 @@
+package com.psychowood.yahapp;
+
+import android.content.SharedPreferences;
+
+/**
+ * Created by gg on 28/08/2016.
+ */
+
+public class App {
+    public static final String APP_PREFS = "YAPappPrefs";
+    public static final String PREF_PSVITAIP = "PSVitaIP";
+}
